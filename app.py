65bbb907b5dd3022b6f0c0666ba8bea78b5672cc
Bryan Pitman <pitman.bryan@gmail.com>
2022-07-27 16:40:31 -0700

    front page and loop works

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+# after working change silly story
+from stories import excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+@app.get('/')
+def show_question():
+    prompts = excited_story.prompts
+
+    return render_template("questions.html", prompts = prompts)
