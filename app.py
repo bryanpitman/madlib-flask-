38ed7f6f25663a58acd19b9414c5ffd38388f3cb
Ryan Rustyn <ryanrustyn@gmail.com>
2022-07-27 17:15:51 -0700

    formatted questions added results updated app.py

--- app.py
+++ app.py
@@ -2,14 +2,23 @@ from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
 # after working change silly story
-from stories import excited_story
+from stories import silly_story as story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
 @app.get('/')
 def show_question():
-    prompts = excited_story.prompts
+    """Generates an input form for story prompts"""
+    prompts = story.prompts
 
     return render_template("questions.html", prompts = prompts)
+
+@app.get('/story')
+def show_results():
+    """Displays the story with user inputs"""
+    result = story.generate(request.args)
+
+    return render_template("results.html", text = result)
